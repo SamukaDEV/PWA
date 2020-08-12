ae0253b6745eeb8405d74e1d3e421860586345d9
Samuel Torres <absfregs@gmail.com>
2020-08-11 21:21:55 -0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,11 +1,13 @@
 var CACHE_NAME = 'static-v1';
 
+var upath = '/PWA/'
+
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
-        // '/',
-        '/PWA/index.html',
+        upath,
+        upath + 'index.html',
         // '/styles.css',
         // '/app.js',
         // '/manifest.js',
